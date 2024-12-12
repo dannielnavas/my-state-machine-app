04c2126d2ba82c106865b3ca93c22335119677f1
Danniel Navas <dannielnavas@gmail.com>
2024-12-12 08:02:21 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+interface StateMachineConfig<StateType, StepNames extends string> {
+  initialStep: StepNames;
+  steps: {
+    [key in StepNames]: {
+      canAdvance: (state: StateType) => boolean;
+    };
+  };
+  views: {
+    [key in StepNames]: React.ComponentType<{
+      state: StateType;
+      setState: React.Dispatch<React.SetStateAction<StateType>>;
+    }>;
+  };
+}
+type WizardState = {
+  name: string;
+  age: number;
+};
+type StepNames = "step1" | "step2" | "confirmation";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
